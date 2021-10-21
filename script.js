30fd7e82d9b38fffc56034952e213cff2887f087
wchizewski <w.chizewski@share.epsb.ca>
2021-10-21 14:47:23 -0600

    thrusdatdya

---
+++ script.js
@@ -0,0 +1,18 @@
+// Draw sunrise assignment by will
+
+// Setup the canvas and the graphics context
+let cnv = document.getElementById("canvas")
+let ctx = cnv.getContext("2d");
+cnv.width = 400;
+cnv.height = 400;
+
+// Store images in variables
+let cloudImg = document.getElementById("cloudImg")
+
+// Draw sky
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, 400, 300);
+
+// Draw grass
+ctx.fillStyle = "green";
+ctx.fillRect = (0, 0, 400, 100);
