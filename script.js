0e69da21d4285659d502e9121742772251044c33
wchizewski <w.chizewski@share.epsb.ca>
2021-10-26 14:38:30 -0600

    tuesday(done)

--- script.js
+++ script.js
@@ -9,20 +9,46 @@ cnv.height = 400;
 // Store images in variables
 let cloudImg = document.getElementById("cloudImg")
 
-// Draw sky
-ctx.fillStyle = "blue";
-ctx.fillRect(0, 0, 400, 300);
+// Global variables
+cloud1X = 140;
+cloud2X = 175;
+sunY1 = 300;
+sunSize = 23;
+sunRed = 255;
+sunGreen = 0
 
-// Draw ground
-ctx.fillStyle = "green";
-ctx.fillRect(0, 300, 400, 100);
 
-// Draw red half sun
-ctx.fillStyle= "red";
-ctx.beginPath();
-ctx.arc(200, 300, 23, 0, Math.PI, true);
-ctx.fill();
+requestAnimationFrame(loop);
+function loop() {
+    // Draw sky
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, 400, 300);
 
-// Draw cloud images
-ctx.drawImage(cloudImg, 140, 105)
-ctx.drawImage(cloudImg, 175, 90)
+    // Draw top half sun
+    ctx.fillStyle = "rgb(" + sunRed + ", " + sunGreen + ", 0";
+    ctx.beginPath();
+    ctx.arc(200, sunY1, sunSize, 0, 2 * Math.PI);
+    ctx.fill();
+
+    // Draw ground
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 300, 400, 100);
+
+    // Draw cloud images
+    ctx.drawImage(cloudImg, cloud1X, 105)
+    ctx.drawImage(cloudImg, cloud2X, 90)
+
+    // Update Variables
+
+    if (sunGreen < 255) {
+        cloud1X--;
+        cloud2X++;
+        sunY1 -= 0.7;
+        sunSize += 0.25
+        sunGreen++;
+    }
+
+
+
+    requestAnimationFrame(loop);
+}
